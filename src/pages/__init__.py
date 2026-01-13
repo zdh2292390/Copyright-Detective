7bd395c6dfba9bd4c0352d9f8a7f18e0244ff3f8
ChangHu <66352520+changhu73@users.noreply.github.com>
2026-01-13 22:59:44 +0800

    update unlearning extraction

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -5,6 +5,7 @@ This module contains page-specific UI components.
 """
 
 from src.pages.legal_cases_display import render_legal_case_display_page
+from src.pages.unlearning_detection import render_unlearning_detection_page
 
-__all__ = ['render_legal_case_display_page']
+__all__ = ['render_legal_case_display_page', 'render_unlearning_detection_page']
 
